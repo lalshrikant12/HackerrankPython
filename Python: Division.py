ac0dbae1bbfc81a7efc1bdb476415f55b08d5958
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-05-16 00:38:09 +0530

    Create Python: Division.py

---
+++ Python: Division.py
@@ -0,0 +1,9 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    division=int(a/b)
+    division1=int(a)/float(b)
+    print(division)
+    print(division1)
