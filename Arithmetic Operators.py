f2c72ad320833933a8f0465c38de03dd82faea46
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-05-16 00:35:37 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    sum=int(a+b)
+    diff=int(a-b)
+    product=int(a*b)
+    print(sum)
+    print(diff)
+    print(product)
+    
