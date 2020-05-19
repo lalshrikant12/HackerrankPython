17e9df657845fc004df561bffc79332796143ffa
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-05-19 22:49:50 +0530

    Create score.py

---
+++ coursera/score.py
@@ -0,0 +1,14 @@
+score=float(input("Enter score"))
+try:
+    score=range(0,1.0)
+except:
+    print("Score out of range")
+
+if score=range(0.9,1.0):
+    print("A")
+elif score=range(0.8,0.9):
+    print("B")
+elif score=range(0.7,0.8):
+    print("C")
+elif score=range(0.6,0.7):
+    print()
