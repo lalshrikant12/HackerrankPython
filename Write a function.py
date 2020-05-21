1289613523a600a21d6d87a3d99cf0493c49455a
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-05-21 23:34:14 +0530

    Update Write a function.py

--- Write a function.py
+++ Write a function.py
@@ -1,9 +1,9 @@
-We add a Leap Day on February 29, almost every four years. The leap day is an extra, or intercalary day and we add it to the shortest month of the year, February.
+"""We add a Leap Day on February 29, almost every four years. The leap day is an extra, or intercalary day and we add it to the shortest month of the year, February.
 In the Gregorian calendar three criteria must be taken into account to identify leap years:
 
 The year can be evenly divided by 4, is a leap year, unless:
 The year can be evenly divided by 100, it is NOT a leap year, unless:
-""The year is also evenly divisible by 400. Then it is a leap year.
+The year is also evenly divisible by 400. Then it is a leap year.
 This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.Source
 
 Task
@@ -30,4 +30,4 @@ Sample Output 0
 False
 Explanation 0
 
-1990 is not a multiple of 4 hence it's not a leap year.""
+1990 is not a multiple of 4 hence it's not a leap year."""
