c638a4059441321737eb64585dff13aff1ee632a
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-06-03 17:35:26 +0530

    Create Mutation.py

---
+++ Mutation.py
@@ -0,0 +1,9 @@
+def mutate_string(string, position, character):
+    list2=list(string)
+    list2[position]=character      
+    return ''.join(list2)
+if __name__ == '__main__':
+    s = raw_input()
+    i, c = raw_input().split()
+    s_new = mutate_string(s, int(i), c)
+    print s_new
